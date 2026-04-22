6c4e882c635b9b1e967a3713f8eacbc3f7baf74a
WajeehaImran22 <iwajeeha02@gmail.com>
2026-04-22 23:04:41 +0500

    Update modal.tsx

--- APP/app/modal.tsx
+++ APP/app/modal.tsx
@@ -1,4 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
+\\modal
+  import { StatusBar } from 'expo-status-bar';
 import { Platform, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
